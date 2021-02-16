8b5e954425d8805fe71560971da5a448e626fa3c
brerickner <breannarickner@gmail.com>
2021-02-16 17:47:14 +0000

    pepfixed amenity

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module creates a Amenity class"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+
+    """ This Amenity class inherits BaseModel """
+
+    name = ""
