b9b8a0353a447236157aa5532e43433e696547ef
brerickner <breannarickner@gmail.com>
2021-02-16 17:16:14 +0000

    adding State class. pepfixed already.

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module creates a State class"""
+
+from .models.base_model import BaseModel
+
+
+class State(BaseModel):
+
+    """ This State class inherits BaseModel """
+
+    name = ""
