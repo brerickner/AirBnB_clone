5111c8b235f392412bc192f1d8b975b98dfecd55
Aleia DeVore <2496@holbertonschool.com>
2021-02-17 01:15:07 +0000

    Console pep8 fixed

--- console.py
+++ console.py
@@ -13,13 +13,14 @@ from models.review import Review
 """ This module creates the console class """
 
 classdict = {'BaseModel': BaseModel,
-            'User': User,
-            'State': State,
-            'City': City,
-            'Amenity': Amenity,
-            'Place': Place,
-            'Review': Review
-            }
+             'User': User,
+             'State': State,
+             'City': City,
+             'Amenity': Amenity,
+             'Place': Place,
+             'Review': Review
+             }
+
 
 class HBNBCommand(cmd.Cmd):
     """ This class creates an interactive console """
@@ -39,7 +40,8 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_create(self, classname):
-        """ Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id """
+        """ Creates a new instance of BaseModel, saves
+        # it (to the JSON file) and prints the id """
         check = 0
         for key, value in classdict.items():
             if classname == key:
@@ -53,22 +55,23 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
 
     def do_show(self, usrinpt):
-        """ Prints the string representation of an instance based on the class name and id """
+        """ Prints the string representation of an
+        # instance based on the class name and id """
         if len(usrinpt) < 1:
             print("** class name missing **")
         else:
             inargs = usrinpt.split()
             # inargs = [class, id]
             if inargs[0] not in classdict.keys():
-               print("** class doesn't exist **")
+                print("** class doesn't exist **")
             elif len(inargs) < 2:
                 print("** instance id missing **")
             else:
                 # find based on id (stored in instid)
                 allInsts = storage.all()
                 check = 0
-                collectvalues =[]
-                for key,value in allInsts.items():
+                collectvalues = []
+                for key, value in allInsts.items():
                     if key == "{}.{}".format(inargs[0], inargs[1]):
                         collectvalues.append(str(value))
                         print(value)
@@ -78,7 +81,8 @@ class HBNBCommand(cmd.Cmd):
                     print("** no instance found **")
 
     def do_destroy(self, usrinpt):
-        """ Deletes an instance based on the class name and id (save the change into the JSON file) """
+        """ Deletes an instance based on the class
+        # name and id (save the change into the JSON file) """
         # inargs[0] = classname, inargs[1] = instid
         inargs = usrinpt.split()
         if not inargs:
@@ -100,7 +104,8 @@ class HBNBCommand(cmd.Cmd):
                     print("** no instance found **")
 
     def do_all(self, usrinpt):
-        """ Prints all string representation of all instances based or not on the class name """
+        """ Prints all string representation of
+        # all instances based or not on the class name """
         clsList = []
         check = 0
         if not usrinpt:
@@ -113,20 +118,18 @@ class HBNBCommand(cmd.Cmd):
             for key, value in allInsts.items():
                 findClass = key.split(".")
                 if usrinpt == findClass[0]:
-                   # clsFound = str(key) + str(value) 
+                    # clsFound = str(key) + str(value)
                     clsList.append(str(value))
             print(clsList)
             check = 1
         elif check == 0:
             print("** class doesn't exist **")
 
-
-
     def do_update(self, usrinpt):
         """ Updates an instance based on the class name and id by adding
-        #or updating attribute (save the change into the JSON file) 
+        # or updating attribute (save the change into the JSON file)
         # format: [classname, instid, attr, value]"""
-        #inargs = [classname, instid, attr, value]
+        # inargs = [classname, instid, attr, value]
         inargs = usrinpt.split(" ", 3)
         if len(inargs) < 1:
             print("** class name missing **")
@@ -143,24 +146,24 @@ class HBNBCommand(cmd.Cmd):
 
             allInsts = storage.all()
             check = 0
-            collectvalues =[]
-            for key,value in allInsts.items():
+            collectvalues = []
+            for key, value in allInsts.items():
                 if key == "{}.{}".format(inargs[0], inargs[1]):
-                    collectvalues.append(value)    
+                    collectvalues.append(value)
                     try:
                         setattr(value, inargs[2], int(inargs[3]))
                         check = 1
                     except:
-                        try: 
+                        try:
                             setattr(value, inargs[2], float(inargs[3]))
                             check = 1
                         except:
-                            try: 
+                            try:
                                 arg3String = str(inargs[3]).split('\"')
-                                if len(arg3String) > 0: 
+                                if len(arg3String) > 0:
                                     setattr(value, inargs[2], arg3String[1])
                                     print(str(value))
-                                    #value.inargs[2] = inargs[3]
+                                    # value.inargs[2] = inargs[3]
                                     check = 1
                                     break
                             except:
@@ -168,10 +171,6 @@ class HBNBCommand(cmd.Cmd):
                                 check = 1
         if check == 0:
             print("** no instance found **")
-        # inst.attr = value
-        # save
-        pass
-
 if __name__ == '__main__':
         """ Enters console loop """
         HBNBCommand().cmdloop()
